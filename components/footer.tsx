78e2045f6f58dcce68e99866e18b42458048bcca
Gary Williams <garycoderX@gmail.com>
2025-08-06 05:18:21 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
         &copy; {new Date().getFullYear()}{" "}
-        <a href="https://github.com/KtanPatel">Ketan</a>. All rights reserved.
+        <a href="https://github.com/garycoderx">Gary</a>. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
