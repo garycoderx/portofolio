9135bdc6927bb78998b85daece89a33cc298a52b
Gary Williams <garycoderX@gmail.com>
2025-08-19 15:27:31 +0200

    update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Gary is a software engineer who specializes in building scalable and extensive websites.',
     siteUrl: 'https://wang-file.vercel.app', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/og.jpg', // Path to your image you placed in the 'static' folder
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
